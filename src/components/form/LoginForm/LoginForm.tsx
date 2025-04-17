b26cd95e23959852fc064dab60d36d756b8c4f2f
Brendhon <brendhon.e.c.m@gmail.com>
2025-04-17 13:09:21 -0300

    fix: improve error handling and user feedback in LoginForm component

--- src/components/form/LoginForm/LoginForm.tsx
+++ src/components/form/LoginForm/LoginForm.tsx
@@ -1,15 +1,17 @@
 'use client';
 
 import { Modal } from '@/components/layout';
+import { Illustration } from '@/components/ui';
+import { useToast } from '@/hooks';
 import { LoginFormData, loginSchema } from '@/schemas';
 import { GeneralModalProps } from '@/types/modal';
 import { Fieldset, Legend } from '@headlessui/react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Mail } from 'lucide-react';
-import Image from 'next/image';
+import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import Input from '../Input/Input';
-import { Illustration } from '@/components/ui';
 
 interface Props extends GeneralModalProps {
   defaultValues?: LoginFormData;
@@ -23,8 +25,35 @@ export default ({ isOpen, onClose, defaultValues }: Props) => {
     },
   });
 
-  const onSubmit = (data: LoginFormData) => {
-    console.log('Dados enviados:', data);
+  // Router
+  const router = useRouter();
+
+  // Toast
+  const { showSuccessToast, showErrorToast } = useToast();
+
+  // Function to handle form submission
+  const onSubmit = async (data: LoginFormData) => {
+    const response = await signIn('credentials', {
+      redirect: false, // Avoid redirecting
+      email: data.email,
+      password: data.password,
+    });
+
+    if (response?.ok) {
+      // Log successful
+      showSuccessToast({ message: 'Login realizado com sucesso!' });
+
+      // Close the modal
+      onClose();
+
+      // Redirect to the dashboard
+      setTimeout(() => router.push('/dashboard'), 2000);
+    } else {
+      // Log failed
+      console.error('Login failed:', response?.error);
+      // Show error toast
+      showErrorToast({ message: 'Email ou senha inválidos' });
+    }
   };
 
   return (
@@ -38,7 +67,7 @@ export default ({ isOpen, onClose, defaultValues }: Props) => {
 
       <Fieldset className="flex flex-col gap-4">
         <Illustration src='login.svg' />
-        
+
         <Legend className="text-20-bold text-dark text-center">
           Login
         </Legend>
