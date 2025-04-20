9f1bee4306df99d6de36f426c855cf844a0c4e27
Brendhon <brendhon.e.c.m@gmail.com>
2025-04-20 11:42:53 -0300

    feat: add Storybook SVG to AvatarPopover and update navigation button

--- src/components/layout/Header/AvatarPopover/AvatarPopover.tsx
+++ src/components/layout/Header/AvatarPopover/AvatarPopover.tsx
@@ -1,8 +1,9 @@
 import FigmaSvg from '@/assets/figma.svg';
 import GithubSvg from '@/assets/github.svg';
+import StorybookSvg from '@/assets/storybook.svg';
 import { HeaderProps } from "@/types/layout";
 import { Button } from '@headlessui/react';
-import { FileCode, Settings, UserIcon } from "lucide-react";
+import { UserIcon } from "lucide-react";
 import Popover from "../../Popover/Popover";
 
 export default ({ onNavigate, onLogout }: Pick<HeaderProps, 'onNavigate' | 'onLogout'>) => (
@@ -15,10 +16,7 @@ export default ({ onNavigate, onLogout }: Pick<HeaderProps, 'onNavigate' | 'onLo
         <FigmaSvg className="inline mr-2 h-auto w-6" /> Figma
       </Button>
       <Button className='popover-li' onClick={() => onNavigate?.(process.env.NEXT_PUBLIC_STORYBOOK_URL || '')}>
-        <FileCode size={24} className="inline mr-2" /> Documentação
-      </Button>
-      <Button className='popover-li' onClick={() => onNavigate?.('/settings')}>
-        <Settings size={24} className="inline mr-2" /> Configurações
+        <StorybookSvg className="inline mr-2 h-auto w-6" /> Storybook
       </Button>
       <Button onClick={() => onLogout?.()} className='border-t border-t-dark-gray p-0 pt-6 w-full text-center popover-li'>
         Sair
