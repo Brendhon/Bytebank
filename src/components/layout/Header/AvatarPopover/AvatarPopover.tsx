32f93c7b5d2fb89ea580eaf15e75403c1e6d8062
Brendhon <brendhon.e.c.m@gmail.com>
2025-04-19 23:45:45 -0300

    feat: add GitHub link to user profile menu and update README with GitHub URL

--- src/components/layout/Header/AvatarPopover/AvatarPopover.tsx
+++ src/components/layout/Header/AvatarPopover/AvatarPopover.tsx
@@ -1,4 +1,5 @@
 import FigmaSvg from '@/assets/figma.svg';
+import GithubSvg from '@/assets/github.svg';
 import { HeaderProps } from "@/types/layout";
 import { Button } from '@headlessui/react';
 import { FileCode, Settings, UserIcon } from "lucide-react";
@@ -7,6 +8,9 @@ import Popover from "../../Popover/Popover";
 export default ({ onNavigate, onLogout }: Pick<HeaderProps, 'onNavigate' | 'onLogout'>) => (
   <Popover pButton={<UserIcon className='popover-trigger rounded-full border border-orange p-2' size={40} />} className='p-4'>
     <div className="flex flex-col gap-2 justify-center items-center">
+      <Button className='popover-li' onClick={() => onNavigate?.(process.env.NEXT_PUBLIC_GITHUB_URL || '')}>
+        <GithubSvg className="inline mr-2 h-auto w-6" /> Github
+      </Button>
       <Button className='popover-li' onClick={() => onNavigate?.(process.env.NEXT_PUBLIC_FIGMA_URL || '')}>
         <FigmaSvg className="inline mr-2 h-auto w-6" /> Figma
       </Button>
